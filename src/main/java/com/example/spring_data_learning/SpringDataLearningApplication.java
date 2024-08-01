bca28b1c4daa49bbf638e594a253275699401a38
sbatthineni <sbatthineni1@gmail.com>
2024-08-01 11:30:54 -0400

    main

---
+++ src/main/java/com/example/spring_data_learning/SpringDataLearningApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_data_learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataLearningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spring_data_learning/SpringDataLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_data_learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
